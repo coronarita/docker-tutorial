ba78e2ca9bb5d1d72d0248b816f48f4473320513
leejunghoon <coronarita1991@gmail.com>
2023-12-12 07:12:43 +0900

    modify linting

--- lesson1/main.py
+++ lesson1/main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World"}
 
-if __name__ == "__main__" : 
+
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
