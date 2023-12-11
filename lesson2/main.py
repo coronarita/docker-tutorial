d7be0c922d72f7846db5daa4b5c82b32064e51cc
leejunghoon <coronarita1991@gmail.com>
2023-12-12 05:44:27 +0900

    publish docker image

--- lesson2/main.py
+++ lesson2/main.py
@@ -8,6 +8,8 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World"}
 
+
+
 if __name__ == "__main__" : 
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
